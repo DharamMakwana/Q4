889cf4d8e94e9dfc26a80b839ff361c320579b9e
anshul46 <89180266+AnshB0712@users.noreply.github.com>
2023-09-25 10:23:37 +0530

    initial commit

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
-const path = require("path");
 
 const serveTemplate = (req, res) => {
   const templateName = req.url.slice(1);
